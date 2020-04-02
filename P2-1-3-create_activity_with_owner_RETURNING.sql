7d28cbdd481a3038c1a24a9c63ecb83632795d0f
yobon <yoann.bonnaventure@iut-rodez.fr>
2020-04-02 14:10:46 +0200

    fix #4 fonction P2-1-3 clause returning

--- P2-1-3-create_activity_with_owner_RETURNING.sql
+++ P2-1-3-create_activity_with_owner_RETURNING.sql
@@ -13,7 +13,7 @@ CREATE OR REPLACE FUNCTION create_activity_with_owner(
         VALUES (a_user_id, a_username, now());
         -- insert the activity
         INSERT INTO activity (id, title, description, creation_date, modification_date, owner_id)
-        VALUES (an_act_id, an_act_title, an_act_descr, now(), now(), a_user_id);
+        VALUES (an_act_id, an_act_title, an_act_descr, now(), now(), a_user_id)
         -- return the created activity
         RETURNING activity;
     $$
